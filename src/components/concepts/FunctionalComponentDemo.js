3e4486ddf03adc576b1aa301c2f228eade4fdd2d
scottjstewart <scottstewart965@gmail.com>
2019-01-12 16:19:23 -0500

    added information to fill the functional component demo section

--- src/components/concepts/FunctionalComponentDemo.js
+++ src/components/concepts/FunctionalComponentDemo.js
@@ -4,7 +4,24 @@ const FunctionalCompnentDemo = () => {
   return (
     <div className="main">
       <div className="mainDiv">
-        <div>Hello React</div>
+        <h1>Functional Components</h1>
+        <p>
+          Functional Components allow you to render information to the web page
+          without having to use or change state.
+        </p>
+        <dl>
+          <dt>Presentational</dt>
+          <dd>
+            Often used for simply rendering a small chunk of code to the dom.
+          </dd>
+          <dt>No 'this' keyword</dt>
+          <dd>Unlike class components, functional ones don't use 'this'</dd>
+          <dt>No state</dt>
+          <dd>These are 'dumb' components for UI.</dd>
+          <dt>return()</dt>
+          <dd>Must return a single element.</dd>
+        </dl>
+        <h1>Functional Syntax versus Arrow Function</h1>
       </div>
     </div>
   );
